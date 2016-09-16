7c9e92cd968f697df902e11a5fced76138d62800
shintoo <seanprapp@gmail.com>
2016-09-16 00:02:56 -0400

    printing assembler version

--- vm.c
+++ vm.c
@@ -1,20 +1,61 @@
 #include "vm.h"
-#include <stlib.h>
+#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
-int main(void) {
-	
-	//adaaff
-	
+int main(int argc, char *argv[]) {
+    if (argc != 2) usage(argv[0]);
+
+    loadInstructions(argv[1]);
 	return 0;
 }
 
-void loadInstructions(const char* filePath) {
-
+void usage(const char *pname) {
+    printf("%s: usage: %s input-file\n", pname, pname);
+    ABORT("no input file provided\n");
 }
 
-void printInstructions(const Instruction* i) {
+void loadInstructions(const char* filePath) {
+    FILE *source = fopen(filePath, "r");
+    char buf[32];
+    char token[32];
+    int i;
 
+    if (!source) ABORT("could not load file %s\n", filePath);
+
+    printf("PL/0 code:\n\n");
+
+    for (i = 0; !feof(source) && i < MAX_CODE_LENGTH; i++) {
+        fgets(buf, 32, source);
+        code[i].op = atoi(strtok(buf, " "));
+        code[i].l = atoi(strtok(NULL, " "));
+        code[i].m = atoi(strtok(NULL, " "));
+        printInstruction(code + i);
+    }
+}
+
+void printInstruction(const Instruction* i) {
+    static const char *NAMES[] = {
+        "__", "LIT", "OPR", "LOD", "STO", "CAL", "INC",
+        "JMP", "JPC", "SIO"
+    };
+    static const char *OPNAMES[] = {
+        "RET", "NEG", "ADD", "SUB", "MUL", "DIV", "ODD",
+        "MOD", "EQL", "NEQ", "LSS", "LEQ", "GTR", "GEQ"
+    };
+    static const char *SIONAMES[] = {
+        "OUT", "INP", "HLT"
+    };
+    static int num = 0;
+
+    printf("%4d  %s\t%d\t%4d\n", num++, 
+            i->op == 2 ?
+                OPNAMES[i->m] : 
+                i->op == 9 ?
+                    SIONAMES[i->m] :
+                    NAMES[i->op]
+            , i->l, i->m
+    );
 }
 
 void fetch(void) {
--- vm.h
+++ vm.h
@@ -3,8 +3,12 @@
 //Brody Nissen
 //Satya Patel
 
-#ifndef "vm.h"
-#define "vm.h"
+#ifndef VM_H
+#define VM_H
+
+#include <stdlib.h>
+
+#define ABORT(...) do { fprintf(stderr, "error: " __VA_ARGS__); exit(EXIT_FAILURE); } while (0);
 
 #define MAX_STACK_HEIGHT 2000
 #define MAX_CODE_LENGTH 500
@@ -18,7 +22,7 @@ typedef struct _instruction {
 
 enum {
 	LIT = 1, OPR, LOD, STO, CAL, INC, JMP, JPC, SIO
-}
+};
 
 int bp = 1;
 int pc = 0;
@@ -29,12 +33,10 @@ Instruction code[MAX_CODE_LENGTH];
 
 Instruction ir;
 
+void usage(const char *pname);
 void loadInstructions(const char* path);
-void printInstructions(const Instruction* i);
+void printInstruction(const Instruction* i);
 void fetch(void);
 void execute(void);
-
-
-
 
 #endif
