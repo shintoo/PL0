10e3abe11a8f72468131a89cb084f49ec256f7c8
Satya Patel <satyadpatel@knights.ucf.edu>
2016-09-22 17:14:33 -0400

    Finishing proj code w/ errors

--- vm.c
+++ vm.c
@@ -59,10 +59,118 @@ void printInstruction(const Instruction* i) {
 }
 
 void fetch(void) {
-	
+	ir = code[pc];
+	pc += 1;
 }
 
 void execute(void) {
+	switch(ir.op) {
+		case 1:
+			sp += 1;
+			stack[sp] = ir.m;
+			break;
+		case 2:
+			switch(ir.m) {
+				case 0:
+					sp = bp - 1;
+					pc = stack[sp + 4];
+					bp = stack[sp+3];
+					break;
+				case 1:
+					stack[sp] *= -1;
+					break;
+				case 2:
+					sp -= 1;
+					stack[sp] += stack[sp+1];
+					break;
+				case 3: 
+					sp -= 1;
+					stack[sp] -= stack[sp+1];
+					break;
+				case 4:
+					sp -= 1;
+					stack[sp] *= stack[sp+1];
+					break;
+				case 5:
+					sp -= 1;
+					stack[sp] /= stack[sp+1];
+					break;
+				case 6:
+					stack[sp] %= 2;
+					break;
+				case 7:
+					stack[sp] -= 1;
+					stack[sp] = stack[sp] % stack[sp+1];
+					break;
+				case 8:
+					sp -= 1;
+					stack[sp] = stack[sp] == stack[sp+1];
+					break;
+				case 9:
+					sp -= 1;
+					stack[sp] = stack[sp] != stack[sp+1];
+					break;
+				case 10:
+					sp -= 1;
+					stack[sp] = stack[sp] < stack[sp+1];
+					break;
+				case 11:
+					sp -= 1;
+					stack[sp] = stack[sp] <= stack[sp+1];
+					break;
+				case 12:
+					sp -= 1;
+					stack[sp] = stack[sp] > stack[sp+1];
+					break;
+				case 13:
+					sp -= 1;
+					stack[sp] = stack[sp] >= stack[sp+1];
+					break;
+			}
+
+		case 3: 
+			sp += 1;
+			stack[sp] = stack[base(ir.l, bp) + ir.m];
+			break;
+		case 4:
+			stack[base(ir.l, bp) + ir.m];
+			sp -= 1;
+			break;
+		case 5:
+			stack[sp+1] = 0;
+			stack[sp+2] = base(L, bp);
+			stack[sp+3] = bp;
+			stack[sp+4] = pc;
+			bp += 1;
+			pc = ir.m;
+			break;
+		case 6:
+			sp += ir.m;
+			break;
+		case 7:
+			pc = ir.m;
+			break;
+		case 8:
+			if (!stack[sp]) pc = ir.m;
+			sp -= 1;
+			break;
+		case 9:
+			switch(ir.m) {
+				case 0: 
+					printf("%d\n", stack[sp]);
+					sp -= 1;
+					break;
+				case 1:
+					sp += 1;
+					scanf("%d", stack[sp]);
+					break;
+				case 2:
+					exit(0);
+					break;
+
+			}
+
+	}		
 
 }
 
