dd7afc572584dd1a5f63c0b1c45185c8e81138cc
shintoo <seanprapp@gmail.com>
2016-10-06 22:10:48 -0400

    uhh

--- lexer.c
+++ lexer.c
@@ -16,33 +16,45 @@
 #include "tokens.h"
 
 enum {
-    text = 0,
-    num, too_long, eof, ident_starts_with_num
+    valid, ident_too_long, ident_starts_with_num, num_val_exceeds_max, invalid_token
 };
 
+int error_num;
+
 typedef struct _token {
-	int which; // text 0, num 1, too long 2, EOF 3, identstaartswithnum 4
+	int which; // obsolete
 	char text[20];
 } token;
-
 
 token getNextToken(FILE *fp);
 token specialcase(FILE *fp, char c);
 token collectToken(FILE *fp, char c);
 void printTokenType(token t);
 token_type findTokenType(token t);
-void printSourceWithAndWithoutComments(FILE *fp);
+void printsource(FILE*, int);
+void getSourceOpts(int, const char **, int*);
 
 int isIdent(char *name);
 int isNum(char *text);
 
-int main(int argc, char *args[]) {
-	FILE *fp = fopen(args[1], "r");	
+int main(int argc, const char *args[]) {
+	FILE *fp;
 	token t;
+    int argvals[2];
 
-    printSourceWithAndWithoutComments(fp);
-    rewind(fp);
-    puts("\n\n");
+    getSourceOpts(argc, args, argvals);
+
+    fp = fopen(args[argvals[1]], "r");
+    switch (argvals[0]) {
+        case 0: // clean
+            printsource(fp, 1);
+            rewind(fp);
+            break;
+        case 1: // comments
+            printsource(fp, 0);
+            rewind(fp);
+            break;
+    }
 
 	t = getNextToken(fp);
 	do {
@@ -56,13 +68,34 @@ int main(int argc, char *args[]) {
 }
 
 void printTokenType(token t) {
-	printf("%s\t\t%2d\n", t.text, findTokenType(t));
+    token_type type = findTokenType(t);
+
+    printf("%s               ", t.text);
+
+    switch (error_num) {
+        case valid:
+            printf("%d\n", type);
+            break;
+        case ident_starts_with_num:
+            puts("Error: identifier begins with number");
+            break;
+        case ident_too_long:
+            puts("Error: identifier too long");
+            break;
+        case invalid_token:
+            puts("Error: invalid token");
+            break;
+        case num_val_exceeds_max:
+            puts("Error: number value exceeds maximum");
+    }
 }
 
 token getNextToken(FILE *fp) {
 	token ret;
 	char c;
 	int in_comment = 0;
+
+    error_num = valid;
 
 	do {
 		c = fgetc(fp);
@@ -178,28 +211,35 @@ token_type findTokenType(token t) {
 }
 
 int isIdent(char *name) {
-	int i = 0;
-	if (!isalpha(name[i++])) return 0;
+	int i = 0;	
+
+    if (!isalpha(name[0]) && !isdigit(name[0])) return 0;
 
 	while(name[i] != '\0') {
-		if(!isalpha(name[i]) && !isdigit(name[i]))
-			return 0;
+		if(!isalpha(name[i]) && !isdigit(name[i])) {
+			if (isalpha(name[0])) error_num = invalid_token;
+            return 0;
+        }
         i++;
 	}
+
+    if (isdigit(name[0])) error_num = ident_starts_with_num;
 	return 1;
 }
 
 int isNum(char *text) {
     int num;
+    char *t = text;
 
-    while (*text)
-        if (!isdigit(*text++))
+    while (*t)
+        if (!isdigit(*t++))
             return 0;
 
-
     num = atoi(text);
-    if (num > 65535)
-        printf("error: number too big: %d\n", num);
+    
+    if (num > 65535) {
+        error_num = num_val_exceeds_max;
+    }
 
     return 1;
 }
@@ -237,6 +277,8 @@ void printsource(FILE *fp, int comments_hidden) {
         }
         fgets(buffer, 80, fp);
     } while (!feof(fp));
+
+    printf("\n\n");
 }
 
 void printSourceWithAndWithoutComments(FILE *fp) {
@@ -244,4 +286,39 @@ void printSourceWithAndWithoutComments(FILE *fp) {
     printsource(fp, 0);
     puts("\nsource code without comments:\n-----------------------------");
     printsource(fp, 1);
+}
+
+void getSourceOpts(int argc, const char **argv, int *argvals) {
+    printf("argc: %d\n", argc);
+    for (int i = 0; i < argc; i++) {
+        printf("%d:  %s, ", i, argv[i]);
+    }
+    if (argc < 3) {
+        argvals[0] = 3;
+        argvals[1] = 1;
+        return;
+    }
+
+    if (!strcmp(argv[1], "--clean")) {
+        argvals[0] = 0;
+        argvals[1] = 2;
+        return;
+    }
+    if (!strcmp(argv[1], "--source")) {
+        argvals[0] = 1;
+        argvals[1] = 2;
+        return;
+    }
+
+    if (!strcmp(argv[2], "--clean")) {
+        argvals[0] = 0;
+        argvals[1] = 1;
+        return;
+    }
+    
+    if (!strcmp(argv[2], "--source")) {
+        argvals[0] = 1;
+        argvals[1] = 1;
+        return;
+    }
 }
