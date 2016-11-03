ff27aed0b639c8953a622930775fce596840f647
Brody Nissen <bnissen64@gmail.com>
2016-11-03 13:51:08 -0400

    added comments

--- parser.c
+++ parser.c
@@ -1,6 +1,11 @@
+
+
+
 void program(void) {
+	// advance and call block
 	advance();
 	block();
+	// if the end token is not a period, error
 	if (token != periodsym)
 		error();
 }
@@ -62,32 +67,44 @@ void block(void) {
 
 void statement(void) {
 	switch (token) {
+		// if it is an identifier
 		case identsym:
 			advance();
+			// must be becomessym
 			if (token != becomessym)
 				error();
+			// get the next token and call expression function
 			advance();
 			expression();
 			break;
+		// if it is a callsym
 		case callsym:
+			// advance and make sure it is an identsym
 			advance();
 			if (token != identsym)
 				error();
 			advance();
 			break;
+		// if it is a beginsym
 		case beginsym:
+			// advance and call the statement funciton
 			advance();
 			statement();
+			// contiue calling statemnt while the token is a semicolon
 			while (token == semicolonsym) {
 				advance();
 				statement();
 			}
 			break;
+		// if it is an if statment
 		case ifsym:
+			// advance and call the condition function
 			advance();
 			condition();
+			// make sure the next token is a thensym
 			if (token != thensym)
 				error();
+			// advance and call the statement function
 			advance();
 			statement();
 			break;
@@ -95,6 +112,7 @@ void statement(void) {
 }
 
 void condition(void) {
+	// for an oddsym
 	if (token == oddsym) {
 		advance();
 		expression();
@@ -108,9 +126,11 @@ void condition(void) {
 }
 
 void expression(void) {
+	// if addition or subtraction
 	if (token == plussym || token == minussym)
 		advance();
 	term();
+	// continue while adding or subtracting
 	while (token == plussym || token == minussym) {
 		advance();
 		term();
@@ -118,6 +138,7 @@ void expression(void) {
 }
 
 void term(void) {
+	// call factor and continue while multiplying or dividing
 	factor();
 	while (token == multsym || token == slashsym) {
 		advance();
@@ -127,12 +148,14 @@ void term(void) {
 
 void factor(void) {
 	switch (token) {
+		// advance if it is an identsym or numbersym
 		case identsym:
 			advance();
 			break;
 		case numbersym:
 			advance();
 			break;
+		// if if is a parenthesis advance and call expression
 		case lparensym:
 			advance();
 			expression();
